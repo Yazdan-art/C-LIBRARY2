a76aa1d1b1b06e4cb4324dd705930b7af93ad14f
yazdan gh <yazdanghezelbash2020@outlook.com>
2024-06-20 10:28:05 -0700

    hi git

---
+++ ShowCase/ShowCase/Program.cs
@@ -0,0 +1,36 @@
+﻿//Console.WriteLine("Hello, World!");
+using ClassLibrary1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int row = 0;
+        do
+        {
+            if (row == 0 || row >= 25)
+            {
+                ResetConsle();
+            }
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) break;
+            Console.WriteLine($"input:{input}");
+            Console.WriteLine("Begins with uppercase?" + $"{(input.StartWithUpper() ? "Yes" : "No")}");
+            Console.WriteLine();
+            row += 4;
+        } while (true);
+        return;
+
+
+       void ResetConsle()
+        {
+            if (row > 0) { Console.WriteLine("Pers any key to continue..."); 
+            Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine($"{Environment.NewLine}Perss<Enter>only to exit ,otherwise, enter a string and press <Enter>:{Environment.NewLine}");
+            row = 3;
+
+        }
+    }
+}
---
+++ blankAsLibrary/ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ClassLibrary1
+{
+    // New non-generic static class
+    public static class StringExtensions
+    {
+        public static bool StartWithUpper(this string? str)
+        {
+            if (string.IsNullOrWhiteSpace(str)) return false;
+            char ch = str[0];
+
+            return char.IsUpper(ch);
+        }
+    }
+}
